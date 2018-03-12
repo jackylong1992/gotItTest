602a92899c848701cd84e808cca2944441cfa888
ntlong <ntlong@humaxdigital.com>
2018-03-12 17:51:13 +0700

    get online account

--- script.js
+++ script.js
@@ -4,9 +4,9 @@
     const LOGOUT = 1;
     var provider = new firebase.auth.GoogleAuthProvider();
     firebase.auth().signInWithPopup(provider).catch(function(error) {
-        console.log(error);
+        //console.log(error);
     }).then(function(result) {
-        console.log(result);
+        //console.log(result);
         if (result.credential) {
           // This gives you a Google Access Token. You can use it to access the Google API.
           var token = result.credential.accessToken;
@@ -25,9 +25,10 @@
     function handleUserAuthentication (user) {
         var userObj;
         var refId = "";
+        var userList;
         // verify in server
         verifyUser().then(function(data) {
-
+            userList = data;
             for (var id in data) {
                 if (data.hasOwnProperty(id)) {
                     // console.log(data[id]);
@@ -42,14 +43,28 @@
         }).then(function() {
             // if not in server, add user
             if (!userObj) {
-                console.log("add user");
+                //console.log("add user");
                 addUser(user);
             } else { // if in server, update
-                console.log("already in server, update user status in refid", refId);
+                //console.log("already in server, update user status in refid", refId);
                 updateUserStatus(LOGIN, refId);
             }
+            getOnlineUser(userList);
         });
 
+    }
+
+    function getOnlineUser(l_userList) {
+        //console.log(l_userList);
+        var onlineArray = [];
+        for (var key in l_userList) {
+            if (l_userList.hasOwnProperty(key)) {
+                if (l_userList[key].loginStatus == 0) {
+                    onlineArray.push(l_userList[key]);
+                }
+            }
+        }
+        console.log("online array", onlineArray);
     }
 
     function updateUserStatus(status, referenceId) {
@@ -75,6 +90,15 @@
             id: user.uid,
             name: user.displayName,
             loginStatus : LOGIN,
+            isFree : true,
+            chatMessageHistory : [{
+                from : "",
+                value : ""
+            }],
+            chatMessageNew : [{
+                from : "",
+                value : ""
+            }]
         });
     }
 
@@ -82,9 +106,7 @@
     // refer: https://firebase.google.com/docs/database/web/read-and-write
     function readFirstDataAtOnce () {
         return firebase.database().ref('/users').once('value').then(function(snapshot) {
-            // var username = (snapshot.val() && snapshot.val().username) || 'Anonymous';
-            // ...
-            console.log(snapshot.val());
+            //console.log(snapshot.val());
           });
     }
 
@@ -92,8 +114,10 @@
     function readData () {
         var promise = new Promise (function (resolve, reject) {
             var starCountRef = firebase.database().ref('/users');
-            starCountRef.on('value', function(snapshot) {
-              console.log(snapshot.val());
+            // starCountRef.on('value', function(snapshot) {
+                // this could be .once()
+            starCountRef.once('value', function(snapshot) {
+              //console.log(snapshot.val());
               resolve(snapshot.val());
             });
         });
